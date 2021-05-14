ca5163453c6bada7cc3dac67b6a63c138684d39a
Monika Szumna <monikaszumna4@gmail.com>
2021-05-14 10:53:38 +0200

    Set up empty Spring Boot Application project

---
+++ src/main/java/pl/zgora/uz/wiea/tna/TnaApplication.java
@@ -0,0 +1,13 @@
+package pl.zgora.uz.wiea.tna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TnaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TnaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/zgora/uz/wiea/tna/TnaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zgora.uz.wiea.tna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TnaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
