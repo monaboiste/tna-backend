2825e0e33f116d1eaabd04ef47a0de565bca0142
Monika Szumna <monikaszumna4@gmail.com>
2021-05-15 18:53:02 +0200

    Provided default in-memory admin user

--- src/main/java/pl/zgora/uz/wiea/tna/config/SecurityConfig.java
+++ src/main/java/pl/zgora/uz/wiea/tna/config/SecurityConfig.java
@@ -1,9 +1,13 @@
 package pl.zgora.uz.wiea.tna.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,14 +19,27 @@ import pl.zgora.uz.wiea.tna.security.CustomUserDetailsService;
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final CustomUserDetailsService userDetailsService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService)
-                .passwordEncoder(passwordEncoder());
+        DaoAuthenticationProvider daoAuth = new DaoAuthenticationProvider();
+        daoAuth.setUserDetailsService(userDetailsService);
+        daoAuth.setPasswordEncoder(passwordEncoder());
+
+        auth.authenticationProvider(daoAuth);
+
+        // Provide default in-memory admin user
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password(passwordEncoder().encode("admin"))
+                .credentialsExpired(false)
+                .accountExpired(false)
+                .accountLocked(false)
+                .roles("ADMIN");
     }
 
     @Override
--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
--- insert default admin with password: admin
-INSERT INTO users(username, password, role)
-VALUES ('admin', '$2a$10$DZSvpb4dGmfiBpbJmCh2oOTv3K4MaKmxEjcSgTBV4qG6Sm5xp3nuK', 'ADMIN');
